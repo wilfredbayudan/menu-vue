1e846cdc87ea51a1f3f17d82971feb69bb7a019b
Wilfred <wilfredbayudan@gmail.com>
2022-02-13 21:52:14 -1000

    Added user create, sessions login/logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
   has_one :business
+  validates :email, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
   get '/hello', to: 'application#hello_world'
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   get '*path',
     to: 'fallback#index',
