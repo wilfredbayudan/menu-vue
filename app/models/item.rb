40a830dd1412a1d1f624978ee148cefd85f1fcc0
Wilfred <wilfredbayudan@gmail.com>
2022-02-16 13:59:17 -1000

    Added item validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   has_many :comments, dependent: :destroy
   belongs_to :category
   validates :category_id, presence: true
-  validates :item, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false, scope: :category_id }
+  validates :item, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false, scope: :category_id, message: "already exists" }
   validates :description, length: { maximum: 150 }
   validates :price, presence: true
 end
