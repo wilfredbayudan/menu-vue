053774543c46a84303811d86e5f097837213afdf
Wilfred <wilfredbayudan@gmail.com>
2022-02-16 23:34:03 -1000

    Added fullWidth option to Floated Content

--- client/src/styles/FloatedContent.js
+++ client/src/styles/FloatedContent.js
@@ -7,16 +7,19 @@ const Wrapper = styled.div`
 `;
 
 const Box = styled.section`
-  width: 50%;
+  width: 100%;
   background-color: #ffffff;
   padding: 30px;
   box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+  @media (min-width: 768px) {
+    width: ${ props => props.fullWidth ? "100%" : "50%"}
+  }
 `;
 
 const FloatedContent = props => {
 
   return (
-    <Wrapper side={props.side}>
+    <Wrapper side={props.side} fullWidth={props.fullWidth}>
       <Box>
         {props.children}
       </Box>
