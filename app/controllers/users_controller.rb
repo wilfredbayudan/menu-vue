6c2b9b1771760b69edc291822fec1ce2fb4081ad
Wilfred <wilfredbayudan@gmail.com>
2022-02-16 14:04:41 -1000

    Refactored, added bang! operators

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,26 @@
 class UsersController < ApplicationController
 
+  before_action :authorize
+  skip_before_action :authorize, only: [:create]
+
   ## POST '/signup'
   def create
-    user = User.create(user_params)
-    if user.valid?
-      session[:user_id] = user.id
-      render json: user, status: :created
-    else
-      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
-    end
+    user = User.create!(user_params)
+    session[:user_id] = user.id
+    render json: user, status: :created
   end
 
   ## GET '/me'
   def show
-    user = User.find_by(id: session[:user_id])
-    if user
-      render json: user
-    else
-      render json: { error: "Unauthorized access" }, status: :unauthorized
-    end
+    user = User.find(session[:user_id])
+    render json: user
   end
 
-  
-
   private
+
+  def authorize
+    super
+  end
 
   def user_params
     params.permit(:email, :password, :password_confirmation, :first_name, :last_name)
