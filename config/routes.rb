ac8466a9708c16ee92979e9c7e4b65cd1475a6d5
Wilfred <wilfredbayudan@gmail.com>
2022-02-16 14:23:29 -1000

    Removed sample route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  
-  get '/hello', to: 'application#hello_world'
+
   post '/signup', to: 'users#create'
   get '/me', to: 'users#show'
   post '/login', to: 'sessions#create'
