91c2d37d05ec9ce13536b874f000d83c65a51093
Wilfred <wilfredbayudan@gmail.com>
2022-02-22 11:21:25 -1000

    Added image to item display

--- client/src/pages/manage/businesses/menu/Item.js
+++ client/src/pages/manage/businesses/menu/Item.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import ItemImagePlaceholder from "../../../../assets/images/item_placeholder.png";
 
 const ItemSquare = styled.div`
   position: relative;
@@ -18,16 +19,33 @@ const ItemContent = styled.div`
   left: 0;
   height: 100%;
   width: 100%;
-  margin: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+const ItemImage = styled.img`
+  max-height: 65%;
+  max-width: 100%;
+`;
+
+const ItemName = styled.span`
+  font-weight: bold;
+`;
+
+const ItemPrice = styled.div`
+  flex-shrink: 0;
+  height: 20px;
 `;
 
 const Item = ({ item, menuManagerState }) => {
   return (
     <ItemSquare>
       <ItemContent>
-        {item.item}<br />
-        {item.description}<br />
-        {item.price}
+        <ItemName>{item.item}</ItemName>
+        <ItemImage src={item.image ? item.image : ItemImagePlaceholder} />
+        <ItemPrice>${item.price}</ItemPrice>
       </ItemContent>
     </ItemSquare>
   )
