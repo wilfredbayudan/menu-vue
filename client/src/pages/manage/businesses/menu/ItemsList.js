ed847e90334dd576c8f6de2fe6f4bf41b2351a00
Wilfred <wilfredbayudan@gmail.com>
2022-02-22 00:49:07 -1000

    Grid layout for items

--- client/src/pages/manage/businesses/menu/ItemsList.js
+++ client/src/pages/manage/businesses/menu/ItemsList.js
@@ -6,6 +6,40 @@ const Container = styled.div`
   padding: 15px;
 `;
 
+const ItemsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(100%, 1fr));
+  @media (min-width: 480px) {
+    grid-template-columns: repeat(auto-fill, minmax(40%, 1fr));
+  }
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(auto-fill, minmax(30%, 1fr));
+  }
+  @media (min-width: 1024px) {
+    grid-template-columns: repeat(auto-fill, minmax(20%, 1fr));
+  }
+  grid-gap: 20px;
+`;
+
+const ItemSquare = styled.div`
+  position: relative;
+  border: 1px solid;
+  box-sizing: border-box;
+  &:before {
+    content: '';
+    display: block;
+    padding-top: 100%;
+  }
+`;
+
+const ItemContent = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+`;
+
 const ItemsList = ({ menuManagerState }) => {
 
   const { business, selectedCategory } = menuManagerState;
@@ -17,12 +51,99 @@ const ItemsList = ({ menuManagerState }) => {
     return null;
   }
 
+  const renderItems = () => {
+    return (
+      <ItemsContainer>
+        <ItemSquare>
+          <ItemContent>
+            Content
+          </ItemContent>
+        </ItemSquare>
+        <ItemSquare>
+          <ItemContent>
+            Content
+          </ItemContent>
+        </ItemSquare>
+        <ItemSquare>
+          <ItemContent>
+            Content
+          </ItemContent>
+        </ItemSquare>
+        <ItemSquare>
+          <ItemContent>
+            Content
+          </ItemContent>
+        </ItemSquare>
+        <ItemSquare>
+          <ItemContent>
+            Content
+          </ItemContent>
+        </ItemSquare>
+        <ItemSquare>
+          <ItemContent>
+            Content
+          </ItemContent>
+        </ItemSquare>
+        <ItemSquare>
+          <ItemContent>
+            Content
+          </ItemContent>
+        </ItemSquare>
+        <ItemSquare>
+          <ItemContent>
+            Content
+          </ItemContent>
+        </ItemSquare>
+        <ItemSquare>
+          <ItemContent>
+            Content
+          </ItemContent>
+        </ItemSquare>
+        <ItemSquare>
+          <ItemContent>
+            Content
+          </ItemContent>
+        </ItemSquare>
+        <ItemSquare>
+          <ItemContent>
+            Content
+          </ItemContent>
+        </ItemSquare>
+        <ItemSquare>
+          <ItemContent>
+            Content
+          </ItemContent>
+        </ItemSquare>
+        <ItemSquare>
+          <ItemContent>
+            Content
+          </ItemContent>
+        </ItemSquare>
+        <ItemSquare>
+          <ItemContent>
+            Content
+          </ItemContent>
+        </ItemSquare>
+        <ItemSquare>
+          <ItemContent>
+            Content
+          </ItemContent>
+        </ItemSquare>
+        <ItemSquare>
+          <ItemContent>
+            Content
+          </ItemContent>
+        </ItemSquare>
+      </ItemsContainer>
+    )
+  }
+
   return (
     <Container>
       <SecondaryTitle title={categoryTitle()} secondaryTitle="Items" />
       {
         selectedCategory ?
-        "Show Items Here"
+        renderItems()
         :
         "Create or select a category to manage menu items."
       }
