b284d481378a2eb44b0e2b7d780571a9ec68d5cc
Wilfred <wilfredbayudan@gmail.com>
2022-02-22 02:06:40 -1000

    Downcase email before saving, downcase email when finding user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_save :downcase_email
+
   validates :first_name, presence: true, length: { maximum: 80 }
   validates :last_name, presence: true, length: { maximum: 80 }
   validates :email, uniqueness: { case_sensitive: false }, email: true
@@ -6,4 +8,10 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 6, maximum: 25 }
   has_many :user_businesses
   has_many :businesses, through: :user_businesses
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
