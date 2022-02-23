82189e259df6a6c253cae6f4a5a24a34691de8f3
Wilfred <wilfredbayudan@gmail.com>
2022-02-22 22:07:35 -1000

    Added instance methods for like and dislike

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,17 @@ class Item < ApplicationRecord
   validates :item, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false, scope: :category_id, message: "already exists" }
   validates :description, length: { maximum: 150 }
   validates :price, presence: true
+
+  def like
+    self.likes += 1
+    self.save
+  end
+
+  def dislike
+    if self.likes > 0
+      self.likes -= 1;
+      self.save
+    end
+  end
+
 end
