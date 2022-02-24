336a4ea3d8e3af41348c1ecf51ec52b5c816461f
Wilfred <wilfredbayudan@gmail.com>
2022-02-24 02:32:21 -1000

    Removed menu id from serializer

--- app/serializers/business_summary_serializer.rb
+++ app/serializers/business_summary_serializer.rb
@@ -1,7 +1,3 @@
 class BusinessSummarySerializer < ActiveModel::Serializer
-  attributes :id, :name, :slug, :description, :image, :menu_id
-
-  def menu_id
-    self.object.menu.id
-  end
+  attributes :id, :name, :slug, :description, :image
 end
