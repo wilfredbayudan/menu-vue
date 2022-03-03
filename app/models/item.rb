1c5ab804e0083230c8f26977d0088eb6054ab101
Wilfred <wilfredbayudan@gmail.com>
2022-03-02 18:30:03 -1000

    Add price validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
   validates :category_id, presence: true
   validates :item, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false, scope: :category_id, message: "already exists" }
   validates :description, length: { maximum: 150 }
-  validates :price, presence: true
+  validates :price, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/, message: "is invalid" }, numericality: { greater_than: 0, less_than: 1000000 }
 
   def like
     self.likes += 1
