0ce304ed0a58e5c02c609f24dd20cf5947a460ba
Wilfred <wilfredbayudan@gmail.com>
2022-03-02 23:43:06 -1000

    Prevent reserved slugs from being added

--- app/models/business.rb
+++ app/models/business.rb
@@ -3,6 +3,6 @@ class Business < ApplicationRecord
   has_many :users, through: :user_businesses
   has_one :menu, dependent: :destroy
   validates :name, presence: true, uniqueness: { case_sensitive: false }
-  validates :slug, presence: true, uniqueness: { case_sensitive: false, message: 'URL already in use, try a new name'  }
+  validates :slug, presence: true, uniqueness: { case_sensitive: false, message: 'URL already in use, try a new name'  }, exclusion: { in: %w[signup me login logout businesses browse how about 401 manage], message: "%{value} is reserved, try a new name." }
   validates :description, length: { maximum: 350 }
 end
