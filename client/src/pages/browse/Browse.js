3f5b30909544de44db9dbeb6cd88ee3415e82027
Wilfred <wilfredbayudan@gmail.com>
2022-03-07 21:53:59 -1000

    Add loading spinner

--- client/src/pages/browse/Browse.js
+++ client/src/pages/browse/Browse.js
@@ -2,15 +2,18 @@ import FloatedContent from "../../styles/FloatedContent";
 import PageTitle from "../../styles/PageTitle";
 import { useEffect, useState } from "react";
 import BusinessList from "./BusinessList";
+import LoaderOverlay from "../../components/LoaderOverlay";
 
 const Browse = ({ appState }) => {
 
   const [businessList, setBusinessList] = useState([]);
   const {setAlert} = appState;
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch('/businesses')
       .then(res => {
+        setLoading(false);
         if (res.ok) {
           res.json().then(json => setBusinessList(json));
         } else {
@@ -19,7 +22,11 @@ const Browse = ({ appState }) => {
             .catch(setAlert({ text: "Something went wrong..." }))
         }
       })
-  }, []);
+  }, [setAlert]);
+
+  if (loading) {
+    return <LoaderOverlay loaderStatus />;
+  };
 
   return (
     <FloatedContent fullWidth>
