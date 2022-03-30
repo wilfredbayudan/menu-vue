1b082b04c7386f5d0da30b3724aed1a33577e294
Wilfred <wilfredbayudan@gmail.com>
2022-03-29 21:32:28 -1000

    Fixed text input width

--- client/src/pages/manage/businesses/menu/ItemSearchForm.js
+++ client/src/pages/manage/businesses/menu/ItemSearchForm.js
@@ -1,23 +1,22 @@
 import styled from "styled-components";
-import ResponsiveTextInput from "../../../../styles/ResponsiveTextInput";
-import FormInput from "../../../../styles/FormInput"
+import TextField from "@mui/material/TextField";
+import FormInput from "../../../../styles/FormInput";
 
 const Container = styled.div`
   margin-bottom: 15px;
 `;
 
 const ItemSearchForm = ({ categoryTitle, search, handleSearchChange }) => {
-
   return (
     <Container>
       <FormInput>
-        <ResponsiveTextInput
-        fullWidth
-        label={`Search ${categoryTitle}...`}
-        name="item"
-        variant="filled"
-        value={search}
-        onChange={handleSearchChange}
+        <TextField
+          fullWidth
+          label={`Search ${categoryTitle}...`}
+          name="item"
+          variant="filled"
+          value={search}
+          onChange={handleSearchChange}
         />
       </FormInput>
     </Container>
