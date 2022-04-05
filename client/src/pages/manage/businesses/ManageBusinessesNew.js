696eafcc1cc12d5848201df372d1b406a609e89c
Wilfred <wilfredbayudan@gmail.com>
2022-04-05 00:45:13 -1000

    Convert state to redux

--- client/src/pages/manage/businesses/ManageBusinessesNew.js
+++ client/src/pages/manage/businesses/ManageBusinessesNew.js
@@ -4,36 +4,40 @@ import ContentNotice from "../../../styles/ContentNotice";
 import { useSearchParams, useNavigate } from "react-router-dom";
 import FormInput from "../../../styles/FormInput";
 import { useState } from "react";
-import FormLabel from "../../../styles/FormLabel"
+import FormLabel from "../../../styles/FormLabel";
 import ResponsiveTextInput from "../../../styles/ResponsiveTextInput";
 import StyledLoadingButton from "../../../styles/StyledLoadingButton";
 import ErrorList from "../../../components/ErrorList";
+import { useSelector, useDispatch } from "react-redux";
+import { login } from "../../../store/userSlice";
 
-const ManageBusinessesNew = ({ appState }) => {
-  
+const ManageBusinessesNew = () => {
+  const dispatch = useDispatch();
+  const userState = useSelector((state) => state.user);
+
   const navigate = useNavigate();
 
   const [searchParams] = useSearchParams();
 
   const newUser = searchParams.get("newUser");
 
-  const { user, setUser } = appState;
+  const { user } = userState;
 
   const [formData, setFormData] = useState({
     name: "",
     description: "",
-    image: ""
+    image: "",
   });
 
   const [loading, setLoading] = useState(false);
 
   const [errors, setErrors] = useState([]);
 
-  const handleChange = e => {
+  const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
-    })
+      [e.target.name]: e.target.value,
+    });
   };
 
   const handleSubmit = (e) => {
@@ -42,43 +46,50 @@ const ManageBusinessesNew = ({ appState }) => {
       method: "POST",
       headers: {
         "content-type": "application/json",
-        "accept": "application/json"
-      }, 
-      body: JSON.stringify(formData)
+        accept: "application/json",
+      },
+      body: JSON.stringify(formData),
     })
-      .then(res => {
+      .then((res) => {
         setLoading(false);
         if (res.ok) {
-          res.json().then(json => {
-            setUser({
-              ...user,
-              businesses: [
-                ...user.businesses,
-                {
-                  business_id: json.id,
-                  name: json.name,
-                  owner: true,
-                  slug: json.slug
-                }
-              ]
-            });
-            navigate(`/manage/businesses/${json.id}/menu${ newUser ? "?newUser=true" : "" }`);
+          res.json().then((json) => {
+            dispatch(
+              login({
+                ...user,
+                businesses: [
+                  ...user.businesses,
+                  {
+                    business_id: json.id,
+                    name: json.name,
+                    owner: true,
+                    slug: json.slug,
+                  },
+                ],
+              })
+            );
+            navigate(
+              `/manage/businesses/${json.id}/menu${
+                newUser ? "?newUser=true" : ""
+              }`
+            );
           });
         } else {
-          res.json().then(json => setErrors(json.errors));
+          res.json().then((json) => setErrors(json.errors));
         }
       })
-      .catch(console.error)
+      .catch(console.error);
   };
 
   const renderNewUserNotice = () => {
     if (!newUser) return null;
     return (
       <ContentNotice>
-        Hey <b>{user.first_name}</b>, welcome to Menu Vue! To get started, let's create a new business.
+        Hey <b>{user.first_name}</b>, welcome to Menu Vue! To get started, let's
+        create a new business.
       </ContentNotice>
-    )
-  }
+    );
+  };
 
   return (
     <FloatedContent fullWidth>
@@ -86,42 +97,43 @@ const ManageBusinessesNew = ({ appState }) => {
       <PageTitle title="Create a Business" />
       <form onSubmit={handleSubmit}>
         <FormInput>
-            <FormLabel htmlFor="business_name">Business Name</FormLabel>
-            <ResponsiveTextInput
-              autoFocus
-              required
-              name="name"
-              id="business_name"
-              variant="filled"
-              value={formData.name}
-              onChange={handleChange}
-            />
+          <FormLabel htmlFor="business_name">Business Name</FormLabel>
+          <ResponsiveTextInput
+            autoFocus
+            required
+            name="name"
+            id="business_name"
+            variant="filled"
+            value={formData.name}
+            onChange={handleChange}
+          />
         </FormInput>
         <FormInput>
-            <FormLabel htmlFor="business_description">Description</FormLabel>
-            <ResponsiveTextInput
-              name="description"
-              multiline
-              rows="4"
-              id="business_description"
-              variant="filled"
-              value={formData.description}
-              onChange={handleChange}
-            />
+          <FormLabel htmlFor="business_description">Description</FormLabel>
+          <ResponsiveTextInput
+            name="description"
+            multiline
+            rows="4"
+            id="business_description"
+            variant="filled"
+            value={formData.description}
+            onChange={handleChange}
+          />
         </FormInput>
         <FormInput>
-            <FormLabel htmlFor="business_image">Image URL</FormLabel>
-            <ResponsiveTextInput
-              name="image"
-              id="business_image"
-              variant="filled"
-              value={formData.image}
-              onChange={handleChange}
-            />
+          <FormLabel htmlFor="business_image">Image URL</FormLabel>
+          <ResponsiveTextInput
+            name="image"
+            id="business_image"
+            variant="filled"
+            value={formData.image}
+            onChange={handleChange}
+          />
         </FormInput>
         <ErrorList errors={errors} />
-        <StyledLoadingButton type="submit" loading={loading}>Save</ StyledLoadingButton>
-
+        <StyledLoadingButton type="submit" loading={loading}>
+          Save
+        </StyledLoadingButton>
       </form>
     </FloatedContent>
   );
--- client/src/pages/manage/businesses/menu/MenuManager.js
+++ client/src/pages/manage/businesses/menu/MenuManager.js
@@ -7,8 +7,9 @@ import ContentNotice from "../../../../styles/ContentNotice";
 import CategoriesList from "./CategoriesList";
 import ItemsList from "./ItemsList";
 import LoaderOverlay from "../../../../components/LoaderOverlay";
-import FindInPageIcon from '@mui/icons-material/FindInPage';
+import FindInPageIcon from "@mui/icons-material/FindInPage";
 import TransparentLoadingButton from "../../../../styles/TransparentLoadingButton";
+import { useSelector } from "react-redux";
 
 const Container = styled.div`
   display: flex;
@@ -19,18 +20,21 @@ const Container = styled.div`
   }
 `;
 
-const MenuManager = ({ appState }) => {
+const MenuManager = () => {
+  const userState = useSelector((state) => state.user);
 
   const { businessId } = useParams();
-  const [business,setBusiness] = useState({});
+  const [business, setBusiness] = useState({});
 
   const [selectedCategory, setSelectedCategory] = useState(null);
 
-  const { user } = appState;
+  const { user } = userState;
 
   const menuManagerState = {
-    business, setBusiness,
-    selectedCategory, setSelectedCategory
+    business,
+    setBusiness,
+    selectedCategory,
+    setSelectedCategory,
   };
 
   const navigate = useNavigate();
@@ -41,51 +45,62 @@ const MenuManager = ({ appState }) => {
 
   useEffect(() => {
     // Only run if businessId found it user.businesses
-    const permission = user.businesses.find(userBusiness => userBusiness.business_id === parseInt(businessId));
+    const permission = user.businesses.find(
+      (userBusiness) => userBusiness.business_id === parseInt(businessId)
+    );
     if (permission) {
       fetch(`/businesses/${businessId}`)
-      .then(res => {
-        if (res.ok) {
-          res.json().then(setBusiness);
-        } else {
-          navigate("/404");
-        }
-      })
-      .catch(console.error)
+        .then((res) => {
+          if (res.ok) {
+            res.json().then(setBusiness);
+          } else {
+            navigate("/404");
+          }
+        })
+        .catch(console.error);
     } else {
-      console.log("No permission")
+      console.log("No permission");
       navigate("/401");
     }
-  }, [businessId, navigate, user.businesses, business.id])
+  }, [businessId, navigate, user.businesses, business.id]);
 
   const renderNewUserNotice = () => {
     if (!newUser) return null;
     return (
       <ContentNotice>
-        Now that you've created your first business, we need to build your menu! Create a new category to begin adding items to your menu.
+        Now that you've created your first business, we need to build your menu!
+        Create a new category to begin adding items to your menu.
       </ContentNotice>
-    )
-  }
+    );
+  };
 
   const handleViewClick = () => {
     navigate(`/${business.slug}`);
-  }
+  };
 
   if (!business.name) return <LoaderOverlay loaderStatus />;
 
   return (
     <FloatedContent fullWidth>
       {renderNewUserNotice()}
-      <PageTitle 
-        title={`${business.name}`} 
-        secondaryTitle="Menu" 
-        sideAction={<TransparentLoadingButton onClick={handleViewClick} startIcon={<FindInPageIcon />}>View Business</TransparentLoadingButton>} />
+      <PageTitle
+        title={`${business.name}`}
+        secondaryTitle="Menu"
+        sideAction={
+          <TransparentLoadingButton
+            onClick={handleViewClick}
+            startIcon={<FindInPageIcon />}
+          >
+            View Business
+          </TransparentLoadingButton>
+        }
+      />
       <Container>
         <CategoriesList menuManagerState={menuManagerState} />
         <ItemsList menuManagerState={menuManagerState} />
       </Container>
     </FloatedContent>
-  )
-}
+  );
+};
 
 export default MenuManager;
